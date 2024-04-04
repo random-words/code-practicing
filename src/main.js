cd6bf9297be6524c6312cc6d6d545387eaf79fe6
random-words <sivko.anton.ua@gmail.com>
2024-04-04 23:51:08 +0300

    library-manager-obj

--- src/main.js
+++ src/main.js
@@ -1,39 +1,102 @@
-const weather = {
-  weathersInCities: [],
+const { nanoid } = require("nanoid");
 
-  sortCitiesByTemperatureUp() {
-    return this.weathersInCities.sort(
-      (firstCity, secondCity) => firstCity.temperature - secondCity.temperature
-    );
-  },
-  sortCitiesByTemperatureDown() {
-    return this.weathersInCities.sort(
-      (firstCity, secondCity) => secondCity.temperature - firstCity.temperature
-    );
-  },
-
-  sortCityByWeatherType(type) {
-    return this.weathersInCities.filter(
-      (weather) => weather.weatherType === type
-    );
-  },
+const STATUS = {
+  AVAILABLE: "available",
+  DELETED: "deleted",
+  PUBLISHING: "publishing",
 };
 
-const addWeatherInCity = ({ cityName, temperature, weatherType }) => {
-  weather.weathersInCities.push({ cityName, temperature, weatherType });
+const libraryManager = {
+  books: [],
+  readers: [],
+
+  addBook({ name, author, releaseDate, genre, status }) {
+    this.books.push({
+      id: nanoid(4),
+      name,
+      author,
+      releaseDate,
+      genre,
+      status,
+    });
+  },
+
+  registerReader({ name, bookList: [], booksHistory: [] }) {
+    this.readers.push({
+      id: nanoid(4),
+      name,
+      bookList: [],
+      booksHistory: [],
+    });
+  },
+
+  giveBookToReader({ readerId, bookId }) {
+    const reader = this.readers.find((reader) => reader.id === readerId);
+    if (reader.bookList.length <= 3) {
+      reader.bookList.push(this.books.find((book) => book.id === bookId));
+      reader.booksHistory.push(this.books.find((book) => book.id === bookId));
+      return;
+    }
+    return `${reader.name}, you have reached your book limit`;
+  },
+
+  returnBook({ readerId, bookId }) {
+    const reader = this.readers.find((reader) => reader.id === readerId);
+    const book = this.books.find((book) => book.id === bookId);
+    if (reader.bookList.find(book)) {
+      reader.bookList.splice(indexOf(book), 1);
+      return;
+    }
+    return `${reader.name}, you don't have this book`;
+  },
+
+  updateBookStatus(id, newStatus) {
+    return (this.books.find((book) => book.id === id).status = newStatus);
+  },
+
+  scearchBooksByAuthor(author) {
+    return this.books.filter((book) => book.author === author);
+  },
+
+  scearchBooksByGenre(genre) {
+    return this.books.filter((book) => book.genre === genre);
+  },
+
+  scearchBooksByStatus(status) {
+    return this.books.filter((book) => book.status === status);
+  },
+
+  //   getBooksByMostPopularGenre(){
+  //     return this.books.filter(book => )
+  //   }
+
+  /**
+   * дддддддддддддддааааааааааааааа....
+   *
+   * реалізація в голові:
+   *
+   * створити об'єкт із ключом "жанр" і
+   * значенням число - кількість повторювань цього жанру в колекції
+   *
+   * потім звіритися, який жанр має найбільшу кількість повторювань і
+   * вивести цей жанр
+   */
+
+  getReadersWithMostBooksRead() {
+    let maxLength = 0;
+    for (const reader of this.readers) {
+      if (reader.booksHistory.length > maxLength) {
+        maxLength = reader.booksHistory.length;
+      }
+    }
+
+    return this.readers.filter((reader) => {
+      reader.booksHistory.length === maxLength;
+    });
+  },
+
+  giveReport() {
+    // getBooksByMostPopularGenre()
+    this.getReadersWithMostBooksRead();
+  },
 };
-
-addWeatherInCity({
-  cityName: "New-York",
-  temperature: 12,
-  weatherType: "cloudy",
-});
-addWeatherInCity({ cityName: "Berlin", temperature: 27, weatherType: "sunny" });
-addWeatherInCity({
-  cityName: "Amsterdam",
-  temperature: -6,
-  weatherType: "hoarfrost",
-});
-
-console.log(weather.sortCitiesByTemperatureDown());
-console.log(weather.sortCityByWeatherType("sunny"));
